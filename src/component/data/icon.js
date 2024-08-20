0ae2a3c8a3aeb157f0a145cc4a1e5e1d0978ac14
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-08-20 11:30:12 +0530

    Update icon.js

--- src/component/data/icon.js
+++ src/component/data/icon.js
@@ -1,73 +1,74 @@
-const icon_image = [{
-    "img":"https://cdn-icons-png.flaticon.com/512/732/732212.png",
+const icon_image = [
+    {
+        "img":"./docker.png",
+        "header":"Docker",
+        "about":"A set of platform as a service products that use OS-level virtualization to deliver software in packages called containers.",
+    },
+    {
+        "img":"./typescript.svg",
+        "header":"TypeScript",
+        "about":"An open-source language which builds on JavaScript, one of the world's most used tools, by adding static type definitions.",
+    },
+    {
+        "img":"./python.png",
+        "header":"Python",
+        "about":"this language is used for task automation,software development,web development and more.",
+    },
+    {
+        "img":"./js.png",
+        "header":"JavaScript",
+        "about":"this language is used both on the client-side and server-side that allows you to make web pages interactive.",
+    },
+    {
+        "img":"./nodejs.webp",
+        "header":"Node js",
+        "about":"it is used to create server-side web applications, and it is perfect for data-intensive applications.",
+    },
+    {
+    "img":"./html.png",
     "header":"HTML",
     "about":"it's used to create the structure of a webpage.",
 },
 {
-    "img":"https://cdn-icons-png.flaticon.com/512/732/732190.png",
-    "header":"CSS",
-    "about":"it's used to set the style in web pages.",
-},
-{
-    "img":"https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
-    "header":"JavaScript",
-    "about":"this language is used both on the client-side and server-side that allows you to make web pages interactive.",
-},
-{
-    "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png",
-    "header":"Python",
-    "about":"this language is used for task automation,software development,web development and more.",
-},
-
-{
-    "img":"https://cdn-icons-png.flaticon.com/512/5968/5968313.png",
+    "img":"./mysql.png",
     "header":"MySql",
     "about":"it's relational database management system based on SQL.",
 },
 {
-    "img":"https://www.freeiconspng.com/thumbs/sql-server-icon-png/sql-server-icon-8.png",
-    "header":"SqlServer",
-    "about":"it's used for storing all the desired information in relational databases.",
-},
-{
-    "img":"https://static-00.iconduck.com/assets.00/django-icon-1606x2048-lwmw1z73.png",
-    "header":"Django",
-    "about":"it's Python web framework that enables rapid development of secure and maintainable websites.",
-},
-{
-    "img":"https://icons.veryicon.com/png/o/business/vscode-program-item-icon/react-3.png",
-    "header":"React js",
-    "about":"It's used for building interactive user interfaces and web applications quickly.",
-},
-{
-    "img":"https://static-00.iconduck.com/assets.00/node-js-icon-1817x2048-g8tzf91e.png",
-    "header":"Node js",
-    "about":"it is used to create server-side web applications, and it is perfect for data-intensive applications.",
-},
-{
-    "img":"https://firebasestorage.googleapis.com/v0/b/fir-57791.appspot.com/o/icons8-express-js-480.png?alt=media&token=c0307adb-35e1-4067-9bd1-4b01878c5ce1",
-    "header":"Express js",
-    "about":"it's Node.js web framework it is used to building web applications and APIs.",
-},
-{
-    "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/993px-Postgresql_elephant.svg.png",
+    "img":"./postgresql.png",
     "header":"PostgreSQL",
     "about":"it's open source relational database management system based on SQL.",
 },
 {
-    "img":"https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_logo_icon_146424.png",
+    "img":"./css.png",
+    "header":"CSS",
+    "about":"it's used to set the style in web pages.",
+},
+
+{
+    "img":"./sqlserver.png",
+    "header":"SqlServer",
+    "about":"it's used for storing all the desired information in relational databases.",
+},
+{
+    "img":"./django.webp",
+    "header":"Django",
+    "about":"it's Python web framework that enables rapid development of secure and maintainable websites.",
+},
+{
+    "img":"./react.png",
+    "header":"React js",
+    "about":"It's used for building interactive user interfaces and web applications quickly.",
+},
+{
+    "img":"./express.png",
+    "header":"Express js",
+    "about":"it's Node.js web framework it is used to building web applications and APIs.",
+},
+{
+    "img":"./mongodb.webp",
     "header":"Mongodb",
     "about":"MongoDB is a document-oriented NoSQL database used for high volume data storage.",
-},
-{
-    "img":"https://docs.infomaniak.cloud/pictures/logo/docker.png",
-    "header":"Docker",
-    "about":"A set of platform as a service products that use OS-level virtualization to deliver software in packages called containers.",
-},
-{
-    "img":"https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg",
-    "header":"TypeScript",
-    "about":"An open-source language which builds on JavaScript, one of the world's most used tools, by adding static type definitions.",
 }
 ];
 export default icon_image;
