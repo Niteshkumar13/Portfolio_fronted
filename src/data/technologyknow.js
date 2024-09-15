0304746293c298c2348eabfdddfe935276111b24
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-09-15 21:38:35 +0530

    Update technologyknow.js

--- src/data/technologyknow.js
+++ src/data/technologyknow.js
@@ -7,7 +7,7 @@ const technologyKnow = [
     },
     {
         "icon":icon.GithubAction,
-        "header": "GithubAction",
+        "header": "GithubActions",
         "about": "GitHub Actions is a continuous integration and continuous delivery (CI/CD) platform that allows you to automate your build, test, and deployment pipeline. ",
     },
     {
